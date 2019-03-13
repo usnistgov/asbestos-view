b2fcb1f60a7f624dbcb76215f766cc2c402d9a46
bill <bmajur@gmail.com>
2019-03-13 10:01:34 -0400

    work

--- src/components/NavigationPage.vue
+++ src/components/NavigationPage.vue
@@ -5,13 +5,35 @@
         <router-link to="/foo">Go to Foo</router-link>
         /
         <router-link to="/bar">Go to Bar</router-link>
+        /
+        <router-link to="/cve">Go to Cve</router-link>
+        /
+        <a href="#" @click="add">Add</a>
+        {{ added }}
     </p>
 
     </div>
 </template>
 
 <script>
+
     export default {
-        name: 'Navigation'
+        data () {
+            return {
+                added: false
+            }
+        },
+        name: 'Navigation',
+        methods: {
+            add () {
+                this.added = true
+                let { routes } = this.$router.options
+                let routeData = routes.find(r => r.path === this.$route.path)
+                routeData.children = [
+                    { path: 'cve', component:  {template: '<span>Cleveland</span>'}}
+                ]
+                this.$router.addRoutes([routeData])
+            }
+        }
     }
 </script>
